4aee00a033182f236316040b363a1e2d8b54c6ad
liyidao <1492433080@qq.com>
2017-06-06 14:32:15 +0800

    上传java类
    github上传的第一个文件

---
+++ Student.java
@@ -0,0 +1,46 @@
+package cn.itcast_06;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	public Student() {
+		super();
+	}
+
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
