4ac21d2e966a10e0e8b128efc57502b8951f4be4
liyidao <1492433080@qq.com>
2017-06-06 13:29:23 +0800

    Update javatest.java

--- javatest.java
+++ javatest.java
@@ -2,6 +2,6 @@ public class JavaTest{
     public static void main(String[] args){
         String name = "Li";
         String age = "35";
-        System.out.println(name+" " age);
+        System.out.println(name + " " + age);
     }    
 }
