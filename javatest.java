def403206988a69280411c15848370643e76899f
liyidao <1492433080@qq.com>
2017-06-06 13:42:09 +0800

    创建java文件
    创建第一个 github java文件 并修改

--- javatest.java
+++ javatest.java
@@ -2,6 +2,7 @@ public class JavaTest{
     public static void main(String[] args){
         String name = "Li";
         String age = "35";
+        String sex = "男";
         System.out.println(name + " " + age);
     }    
 }
